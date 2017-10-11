8fb405e1e34537d302645406ef13c27dde76648a
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2017-10-11 14:31:53 +0200

    Add UI elements

---
+++ PartyRockApp/PartyRockApp/Controller/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by dvt on 2017/10/11.
+//  Copyright © 2017 dvt. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
